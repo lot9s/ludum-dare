41b2432b32af6591b2000c06788819beb5031fb1
lot9s <lordofthenines@gmail.com>
2017-04-22 14:00:27 -0400

    ludum dare 38 - added the beginnings of a level

--- 38/js/game.js
+++ 38/js/game.js
@@ -1,3 +1,6 @@
+let avatar = null;
+let ground = null;
+
 window.onload = function() {
   /*
     Note that this html file is set to pull down Phaser 2.5.0 from the
@@ -8,17 +11,40 @@ window.onload = function() {
   */
   var game = new Phaser.Game(640, 480, Phaser.AUTO, 'game', {
     preload: preload, 
-    create: create 
+    create: create,
+    update: update
   });
 
   function create () {
-    //var logo =
-    //  game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    // --- for debugging purposes only ---
+    game.stage.backgroundColor = "#00ffff";
 
-    //logo.anchor.setTo(0.5, 0.5);
+    // add sprites
+    game.add.sprite(0, 0, 'background');
+    ground = game.add.sprite(100, 330, 'ground');
+    avatar = game.add.sprite(312, 250, 'avatar');
+
+    // add physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([ground, avatar]);
+
+    // adjust settings for ground
+    ground.body.immovable = true;
+
+    // adjust settings for avatar
+    avatar.body.bounce.y = 0.1;
+    avatar.body.gravity.y = 100;
+    avatar.body.collideWorldBounds = true;
   }
 
   function preload () {
-    //game.load.image('logo', 'phaser.png');
+    game.load.image('background', 'res/img/background.png');
+    game.load.image('ground', 'res/img/ground.png');
+    game.load.image('avatar', 'res/img/avatar.png');
+  }
+
+  function update() {
+    // add collision detection
+    game.physics.arcade.collide(avatar, ground);
   }
 };
