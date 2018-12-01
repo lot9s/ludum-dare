d73a652ee07d896df8f72527fa2bdd45315b9b6d
lot9s <lordofthenines@gmail.com>
2018-12-01 00:06:09 -0500

    ludum dare 43 positioned html elements

--- 43/js/game.js
+++ 43/js/game.js
@@ -1,10 +1,25 @@
-/* game object */
-let game = new Phaser.Game(640, 480, Phaser.AUTO, 'test', {
-    preload: preload,
-    create: create,
-    update: update,
-    render: render
-});
+/* phaser 3 configuration */
+let config = {
+    width: 640,
+    height: 480,
+    parent: 'game',
+    scene: {
+        preload: preload,
+        create: create
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            }
+        }
+    },
+    type: Phaser.AUTO, 
+}
+
+/* phaser 3 game */
+let game = new Phaser.Game(config);
 
 function create() {
 
