3f7c53d793ed81b4310da9c335857845ef8af100
lot9s <lordofthenines@gmail.com>
2018-11-30 23:21:53 -0500

    ludum dare 43 initial commit

---
+++ 43/js/game.js
@@ -0,0 +1,23 @@
+/* game object */
+let game = new Phaser.Game(640, 480, Phaser.AUTO, 'test', {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+});
+
+function create() {
+
+}
+
+function preload() {
+    
+}
+
+function render() {
+
+}
+
+function update() {
+
+}
